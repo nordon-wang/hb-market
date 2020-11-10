1d6d6843c3f8f3d5322746ca9c72c6d7f25d8474
wangyao511 <wangyao511@hellobike.com>
2020-11-10 20:43:20 +0800

    add github

--- dist/h5/components/test/test.js
+++ dist/h5/components/test/test.js
@@ -2,7 +2,7 @@ import { Component } from "@tarojs/taro-h5";
 import Nerv from "nervjs";
 import { View, Text } from '@tarojs/components';
 import './test.less';
-export default class Timer extends Component {
+export default class Test extends Component {
   render() {
     return <View className="timer">
         <Text>'aaaa'</Text>
