676de1fd3e06ee086ad49cc54687a0481021117f
wangyao511 <wangyao511@hellobike.com>
2020-11-10 20:28:27 +0800

    add github

---
+++ dist/h5/components/test/test.js
@@ -0,0 +1,11 @@
+import { Component } from "@tarojs/taro-h5";
+import Nerv from "nervjs";
+import { View, Text } from '@tarojs/components';
+import './test.less';
+export default class Timer extends Component {
+  render() {
+    return <View className="timer">
+        <Text>'aaaa'</Text>
+      </View>;
+  }
+}
---
+++ dist/weapp/index.js
@@ -0,0 +1,4 @@
+import Test from './components/test/test'
+
+
+export default Test
